f204368a5ce160b7fd932540e4ad34dd4abe906e
subiki <karmicj@gmail.com>
2026-08-19 02:00:37 -0700

    fix: report gamma auth bypass as forbidden

--- src/environmentReadiness.js
+++ src/environmentReadiness.js
@@ -19,7 +19,7 @@ const expectedSchemas = {
 };
 
 const isolatedRuntimeEnvironments = new Set(['jfl', 'dru', 'gamma']);
-const testAuthRuntimeEnvironments = new Set(['jfl', 'dru', 'gamma']);
+const testAuthRuntimeEnvironments = new Set(['jfl', 'dru']);
 const knownRuntimeEnvironments = new Set(Object.keys(fixedExpectedSupabaseProjectRefs));
 
 function normalizeSupabaseUrl(value) {
