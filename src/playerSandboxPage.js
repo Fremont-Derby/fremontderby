b424fb507fedd58c7a71b17f86c9394165a71991
ctf-gooo-002 <drew98103@gmail.com>
2026-09-01 22:05:48 -0700

    Use the shipped scorecard controller in the sandbox

--- src/playerSandboxPage.js
+++ src/playerSandboxPage.js
@@ -1,10 +1,9 @@
 import { renderRackLedgerScorecardPage } from './rackLedgerScorecard.js';
 import { sandboxRackLedgerAdapterSource } from './sandboxRackLedgerAdapter.js';
-import { applyScorecardProgressionRuntime } from './scorecardProgressionRuntime.js';
 
 export function renderPlayerSandboxPage() {
-  return applyScorecardProgressionRuntime(renderRackLedgerScorecardPage({
+  return renderRackLedgerScorecardPage({
     title: 'Season 1 War Games · Score Match · Fremont Derby',
     adapterSource: sandboxRackLedgerAdapterSource(),
-  }));
+  });
 }
