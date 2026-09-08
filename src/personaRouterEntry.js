ff95a449b99ac9e156e912927f7cf4b319612f09
subiki <karmicj@gmail.com>
2026-09-08 12:36:52 -0700

    fix(qa): apply mission game shell UX

--- src/personaRouterEntry.js
+++ src/personaRouterEntry.js
@@ -2,6 +2,7 @@ import baseRouterEntry from './routerEntry.js';
 import { routeJflSeasonSchedule } from './jflSeasonScheduleHttp.js';
 import { enhanceFinishedScheduleBreakdown } from './finishedScheduleEnhancer.js';
 import { routeQaMissionCampaign } from './qaMissionCampaign.js';
+import { enhanceQaMissionGameUx } from './qaMissionGameUxEnhancer.js';
 import { routeQaScorecard } from './qaScorecardRouteEnhancer.js';
 import { enhanceQaResultUx } from './qaResultUxEnhancer.js';
 import { injectTestPersonaControls } from './testPersonaEnhancer.js';
@@ -13,7 +14,7 @@ export default {
 
   async fetch(request, env, ctx) {
     const qaMissionResponse = routeQaMissionCampaign(request, env);
-    if (qaMissionResponse) return qaMissionResponse;
+    if (qaMissionResponse) return enhanceQaMissionGameUx(qaMissionResponse, request, env);
 
     const qaScorecardResponse = await routeQaScorecard(request, env);
     if (qaScorecardResponse) return enhanceQaResultUx(qaScorecardResponse, request, env);
