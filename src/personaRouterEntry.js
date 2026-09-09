447f7e80bca40223abbf862f8924f8d000e88c0f
subiki <karmicj@gmail.com>
2026-09-08 17:46:55 -0700

    feat(qa): route captain add-players mission through product

--- src/personaRouterEntry.js
+++ src/personaRouterEntry.js
@@ -6,6 +6,7 @@ import { enhanceQaMissionGameUx } from './qaMissionGameUxEnhancer.js';
 import { enhanceQaPlayerNextMatchMission, routeQaPlayerNextMatchMission } from './qaPlayerNextMatchMission2.js';
 import { enhanceQaPlayerAvailabilityMission, routeQaPlayerAvailabilityMission } from './qaPlayerAvailabilityMission.js';
 import { enhanceQaNextMatchRecovery, routeQaNextMatchRecovery } from './qaNextMatchRecoveryEnhancer.js';
+import { enhanceQaCaptainAddPlayersMission, routeQaCaptainAddPlayersMission } from './qaCaptainAddPlayersMission.js';
 import { routeQaScorecard } from './qaScorecardRouteEnhancer.js';
 import { enhanceQaResultUx } from './qaResultUxEnhancer.js';
 import { injectTestPersonaControls } from './testPersonaEnhancer.js';
@@ -16,6 +17,9 @@ export default {
   ...baseRouterEntry,
 
   async fetch(request, env, ctx) {
+    const captainAddPlayersResponse = await routeQaCaptainAddPlayersMission(request, env);
+    if (captainAddPlayersResponse) return captainAddPlayersResponse;
+
     const playerNextMatchRecoveryResponse = routeQaNextMatchRecovery(request, env);
     if (playerNextMatchRecoveryResponse) return playerNextMatchRecoveryResponse;
 
@@ -38,6 +42,7 @@ export default {
     if (scheduleResponse) return scheduleResponse;
 
     let response = await baseRouterEntry.fetch(request, env, ctx);
+    response = await enhanceQaCaptainAddPlayersMission(response, request, env);
     response = await enhanceQaPlayerAvailabilityMission(response, request, env);
     response = await enhanceQaPlayerNextMatchMission(response, request, env);
     response = await enhanceQaNextMatchRecovery(response, request, env);
