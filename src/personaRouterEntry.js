8e007911105bbe88bd9f147be8db1271f76bf0fd
subiki <karmicj@gmail.com>
2026-09-08 18:17:21 -0700

    fix(qa): apply staged captain mission framing

--- src/personaRouterEntry.js
+++ src/personaRouterEntry.js
@@ -7,6 +7,7 @@ import { enhanceQaPlayerNextMatchMission, routeQaPlayerNextMatchMission } from '
 import { enhanceQaPlayerAvailabilityMission, routeQaPlayerAvailabilityMission } from './qaPlayerAvailabilityMission.js';
 import { enhanceQaNextMatchRecovery, routeQaNextMatchRecovery } from './qaNextMatchRecoveryEnhancer.js';
 import { enhanceQaCaptainAddPlayersMission, routeQaCaptainAddPlayersMission } from './qaCaptainAddPlayersMission.js';
+import { enhanceQaCaptainMissionFraming } from './qaCaptainMissionFramingEnhancer.js';
 import { routeQaScorecard } from './qaScorecardRouteEnhancer.js';
 import { enhanceQaResultUx } from './qaResultUxEnhancer.js';
 import { injectTestPersonaControls } from './testPersonaEnhancer.js';
@@ -43,6 +44,7 @@ export default {
 
     let response = await baseRouterEntry.fetch(request, env, ctx);
     response = await enhanceQaCaptainAddPlayersMission(response, request, env);
+    response = await enhanceQaCaptainMissionFraming(response, request, env);
     response = await enhanceQaPlayerAvailabilityMission(response, request, env);
     response = await enhanceQaPlayerNextMatchMission(response, request, env);
     response = await enhanceQaNextMatchRecovery(response, request, env);
