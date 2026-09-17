786afc56373ef8336b34e45decb164125ea0ad60
ctf-gooo-003 <slutsky5544@gmail.com>
2026-09-17 13:31:20 -0700

    DRU: highlight requested player on /players from ?player=
    Tracks #2350. Prefills directory search from the query and marks matching rows without exposing player_id or contact fields.

--- test/players-directory-lists-standings.test.js
+++ test/players-directory-lists-standings.test.js
@@ -11,3 +11,14 @@ test('player directory fetches public individual standings', () => {
   const script = html.split('<script>')[1].split('</script>')[0];
   new vm.Script(script);
 });
+
+test('player directory prefills search and marks requested player from ?player=', () => {
+  const html = renderPlayersDirectoryPage();
+  assert.match(html, /URLSearchParams\(location\.search\)\.get\('player'\)/);
+  assert.match(html, /data-requested-player/);
+  assert.match(html, /searchEl\.value = requestedPlayer/);
+  assert.doesNotMatch(html, /player_id/);
+  assert.doesNotMatch(html, /\bemail\b|phone_number|contact_number/);
+  const script = html.split('<script>')[1].split('</script>')[0];
+  new vm.Script(script);
+});
